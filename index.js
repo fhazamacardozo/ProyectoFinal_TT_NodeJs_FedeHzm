65953b264c9073021f5b0b9d93478fc2954e8115
Federico Hazama <federicohzm@gmail.com>
2025-12-01 19:54:56 -0300

    readme en raiz

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', ProductRoutes);
+app.get('/', (req, res) => {
+    res.send(`
+        <h2>API Endpoints</h2>
+        <ul>
+        <li>GET /api/products - Listar productos</li>
+        <li>GET /api/products/:id - Obtener producto por ID</li>
+        <li>POST /api/products - Crear producto</li>
+        <li>DELETE /api/products/:id - Eliminar producto</li>
+        </ul>
+    `);
+    });
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Recurso no encontrado o ruta inválida' });
 });
